6b3ee2b0be044d9bd4a201b3be3d5ab1b46bb9a5
Ander <anderescaray1@gmail.com>
2026-04-30 09:31:58 +0200

    chore/data_processing pipeline

---
+++ src/amazon_recsys/pipelines/data_processing/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 1.3.1
+"""
+
+from kedro.pipeline import Node, Pipeline  # noqa
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return Pipeline([])
