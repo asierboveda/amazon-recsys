cbd83cdf881a45864f18cc5eb27d8ea3beb35cc3
asierboveda <asierbovedalopez@gmail.com>
2026-04-30 12:01:30 +0200

    feat: add data processing

--- src/amazon_recsys/pipelines/data_processing/nodes.py
+++ src/amazon_recsys/pipelines/data_processing/nodes.py
@@ -1,4 +1,25 @@
-"""
-This is a boilerplate pipeline 'data_processing'
-generated using Kedro 1.3.1
-"""
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col
+from pyspark.ml.feature import StringIndexer
+
+def clean_data(df: DataFrame) -> DataFrame:
+    """Filtra las columnas necesarias y elimina valores nulos."""
+    cleaned_df = df.select(
+        col("user_id"),
+        col("parent_asin"),
+        col("rating").cast("float")
+    ).dropna()
+
+    return cleaned_df
+
+def index_features(df: DataFrame) -> DataFrame:
+    """Convierte los IDs alfanuméricos a enteros usando StringIndexer (Obligatorio para ALS)."""
+    # 1. Convertir user_id (texto) a user_id_num (entero)
+    user_indexer = StringIndexer(inputCol="user_id", outputCol="user_id_num", handleInvalid="skip")
+    df_users = user_indexer.fit(df).transform(df)
+
+    # 2. Convertir parent_asin (texto) a item_id_num (entero)
+    item_indexer = StringIndexer(inputCol="parent_asin", outputCol="item_id_num", handleInvalid="skip")
+    df_final = item_indexer.fit(df_users).transform(df_users)
+
+    return df_final
--- src/amazon_recsys/pipelines/data_processing/pipeline.py
+++ src/amazon_recsys/pipelines/data_processing/pipeline.py
@@ -1,10 +1,20 @@
-"""
-This is a boilerplate pipeline 'data_processing'
-generated using Kedro 1.3.1
-"""
-
-from kedro.pipeline import Node, Pipeline  # noqa
-
+from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import clean_data, index_features
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    return pipeline(
+        [
+            node(
+                func=clean_data,
+                inputs="amazon_reviews",           # Lee del catalog.yml (el parquet original)
+                outputs="cleaned_reviews",         # Se guarda temporalmente en RAM
+                name="clean_data_node",
+            ),
+            node(
+                func=index_features,
+                inputs="cleaned_reviews",          # Toma el dato limpio del paso anterior
+                outputs="preprocessed_video_games",# Guarda el resultado final en catalog.yml
+                name="index_features_node",
+            ),
+        ]
+    )
